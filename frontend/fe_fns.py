fd9efad604b9a38ea772cabae5bbed7e3d12f5bc
morezio <m@morez.io>
2024-08-01 12:55:50 -0600

    sketches fns, ignore

---
+++ frontend/fe_fns.py
@@ -0,0 +1,16 @@
+
+# unzip into a dir
+def unzip():
+    pass
+
+# validate that the file is supported
+def image_is_supported():
+    pass
+
+# given the dimensions selected, 
+def user_dims_selection():
+    pass
+
+def hash_picture():
+    pass
+
