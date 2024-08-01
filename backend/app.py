5750ae20dc7e6e602fadfadd4d8e40a08194b33e
morezio <m@morez.io>
2024-08-01 15:23:41 -0600

    changes name of core fn

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
-
+from be_fns import resize_image
 # this is to be served with gunicorn at runtime
 app = Flask(__name__)
 
+# receives a path and dimensions to transform
+# returns a path if success
+# empty or crashes if not
 @app.route("/create_thumbnail", methods=["POST"])
 def create_thumbnail():
     # receive the name of the picture and dimensions from the body
@@ -10,7 +13,7 @@ def create_thumbnail():
     filename = thumbnail_request['filename']
     new_dimensions = thumbnail_request['dimensions']
     # returns the string of the new filename; filename_dimensionXdimension.ext;; just the path
-    resized_image = resize_picture(filename, new_dimensions)
+    resized_image = resize_image(filename, new_dimensions)
     # add a try-except
     # response = {thumbnail_request + filename:path} | {thumbnail_request + filename:''};; we need to log errors at least
     thumbnail_response = {
