01d9c6a5c672011fc713ae9db53be50d176b3dbe
morezio <m@morez.io>
2024-08-04 10:56:07 -0600

    merge into fe

---
+++ frontend/components.py
@@ -0,0 +1,144 @@
+## Right now both layout and components live here to not make a mess of files
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+
+src_logo = '/storimages/frontend/assets/storimages_logo_by_morezio_white.svg'
+
+# the contents of a file with instructions; a markdown file
+user_instructions = None
+with open('/storimages/frontend/assets/instructions.md') as hh:
+    user_instructions = hh.read()
+
+# holder for contents
+instructions = dcc.Markdown(
+    id='instructions_markdown',
+    className='markdown_text',
+    children=[user_instructions]
+    # style={"display": "none"}
+)
+
+# div separated because I felt it unreadable rn, might re-int
+instructions_division = html.Div(
+    id='instructions_division', children=[instructions]
+)
+
+# toggle on-off display of instructions
+instructions_button = dbc.Col(
+    children=[
+        html.Button('Instructions', id='instructions_button', className='action-button')
+    ]
+)
+
+submit_button_div = html.Div(id='submit_button_div',children=[])
+
+submit_button = [dbc.Col(
+    children=[html.Button('Submit', id='submit_button', className='action-button')])]
+
+# must appear only when processing is done
+download_button = dbc.Col(
+    children=[
+        html.Button('Download results', id='download_button', style={'display':'none'})
+    ]
+)
+
+# to make sure the user wants to proceed
+submit_button = html.Button('Submit', id='submit_button', n_clicks=0)
+
+# area where the upload display should be
+upload_column = dbc.Col(align='center',children=[
+    html.Div(
+        children=[
+            # either zip or a directory of pictures or a selection of them
+            dcc.Upload( 
+                id='upload_area',
+                className='highlight',
+                children=[
+                    html.Div([
+                        html.H2(
+                            "Drag & drop or click here to select your pictures",
+                            className='context-text'
+                        )
+                    ])
+                ]#, multiple=False # allowing for multiple risk;; mind how to handle it 
+            )
+        ]
+    )
+], width=6)
+
+logo = html.Div(
+    children=[
+        html.Img(src=src_logo, className='svg-image')
+    ],
+    style={
+        'position': 'relative',
+        'height': '100vh',
+        'width': '100vw'
+    }
+)
+
+preset_dimensions_div = dbc.Col(
+    align='center',
+    children=[html.Div(id='preset_dimensions_div')], width=6)
+
+preset_dimensions = dcc.Dropdown(
+            id='thumbnail_sizes_dropdown',
+        options=[
+            'Photo galleries = 120x90',
+            'Video = 160x120',
+            'Social media profile picture = 100x100',
+            'Online store - small = 80x80',
+            'Online store - large = 150x150',
+            'Icons = 96x96'
+        ],
+        placeholder='Please, select 1 option to proceed')
+
+preview_div = html.Div(id='preview_list', className='list-container', children=[])
+
+error_markdown = dcc.Markdown('## Please refresh the webpage. Your input was \
+                              not valid. Only upload .zip or pics of supported format', style={'color':'red'})
+
+auto_download = html.Div([dcc.Download(id="download_image")])
+
+##############
+### Layout ###
+##############
+
+# all within a major container; might have inner containers
+storimages_layout = dbc.Container(
+    [
+        dbc.Row([html.H1("StorImages", className="text-gradient")]),
+        dbc.Row(
+            [
+                dbc.Col([
+                    html.Div([
+                        # upload_column is full grid
+                        dbc.Row([upload_column, 
+                                 html.Br(),
+                                 preview_div, 
+                                 html.Br(),
+                                 preset_dimensions_div,
+                                 html.Br(),
+                                 submit_button_div
+                                 ]),
+                        dbc.Row([html.Br()]), # placeholder for loading bar
+                        dbc.Row([html.Br()]), # spacing
+                        dbc.Row([auto_download]), # spacing
+                        # download button and legend
+                        # dbc.Row([download_button]),
+                        # dbc.Row([update_legend_div]),
+                    ], className='container') # highlight box
+                ])
+            ]
+                ),
+        dbc.Row([html.Br()]),
+        # dbc.Row([dbc.Col([preview_div])]),
+        # dbc.Row([dbc.Col([submit_button_div],className='action-button-column')]),
+        dbc.Row([html.Br()]),
+        dbc.Row([html.Br()]),
+        # dbc.Row([dbc.Col([download_button])]),
+        dbc.Row([html.Br()]),
+        dbc.Row([html.Br()]),
+        # dbc.Row([dbc.Col([instructions_button],className='action-button-column')]),
+        dbc.Row([instructions_division])
+    ], fluid=True
+)
---
+++ frontend/fe_fns.py
@@ -0,0 +1,141 @@
+import zipfile, io, base64, os, asyncio, aiohttp
+from dash import html, dcc
+
+pillow_supported = [".bmp",".dib",".dcx",".eps",".ps",".gif",".icns",
+                        ".ico",".im",".jpg",".jpeg",".jpe",".j2k",".j2p",
+                        ".jp2",".jpc",".jpf",".jpx",".mpo",".msp",".pcx",
+                        ".png",".ppm",".pbm",".pgm",".psd",".sgi",".tiff",
+                        ".tif",".webp",".xbm",".xpm"]
+shared_directory = '/storimages/data'
+
+# returns decoded contents and its filename in a tuple
+def uploaded_content_handler(contents, filename):
+    content_type, content_string = contents.split(',')
+    decoded_content = base64.b64decode(content_string)
+    return decoded_content, filename
+
+# unzips in memory
+def unzip(decoded_content_that_is_zip):
+    file_list = None
+    with zipfile.ZipFile(io.BytesIO(decoded_content_that_is_zip)) as z:
+        file_list = z.namelist()
+    return file_list
+        
+# extracts to a provisional dir
+def extract_zip_to_disk(decoded_content_that_is_zip, filename):
+    dir_name = filename.replace('.zip','')
+    extraction_dir = os.path.join(shared_directory, dir_name)
+
+    with zipfile.ZipFile(io.BytesIO(decoded_content_that_is_zip)) as z:
+        z.extractall(shared_directory)
+    files_paths = [os.path.join(extraction_dir, image) for image in os.listdir(extraction_dir)]
+    items_processable = processable_items(files_paths)
+    for item in items_processable:
+        os.chmod(item, 0o666)  # accessible by anyone
+    os.chmod(extraction_dir, 0o777)  # dir is also accessible
+    return extraction_dir
+
+#  Compress the directory into a ZIP file
+def compress_into_zip(zip_file_path, responses): # responses = list of paths in responses
+    with zipfile.ZipFile(zip_file_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        # file path is the abs path to current path
+        for resized_pic in responses:
+            zipf.write(resized_pic)
+    return zip_file_path
+
+# saves the picture uploaded
+def save_uploaded_picture(decoded_contents, filename):
+    uploaded_picture_path = os.path.join(shared_directory,filename)
+    
+    if os.path.exists(uploaded_picture_path):
+        os.remove(uploaded_picture_path)
+    
+    with open(uploaded_picture_path, 'wb') as f:
+        f.write(decoded_contents)
+    os.chmod(uploaded_picture_path, 0o666)  # accessible by anyone
+    os.chmod(shared_directory, 0o777)  # dir is also accessible
+    return uploaded_picture_path
+
+# validate that the file is supported
+# file_list has to be processed by this fn
+def file_is_supported(filename, extensions=pillow_supported):
+    # returns True for any item in iterable (of any(y))
+    # else, iterable empty, False
+    return any(filename.lower().endswith(ext) for ext in extensions)
+
+# returns only those files in zip that are supported
+def processable_items(file_list):
+    items_to_process = []
+    for image_filename in file_list:
+        if file_is_supported(image_filename):
+            items_to_process.append(image_filename)
+    return items_to_process
+
+# will return a list of files to show to the user before unblocking the submit 
+# button
+def show_items(filename_or_list):
+    if isinstance(filename_or_list, list):
+        show_items = html.Div([
+        html.H3("Processable items"),
+        html.Ul([html.Li(filename) for filename in filename_or_list], id="list-container")])
+        return show_items
+    elif isinstance(filename_or_list, str):
+        show_items = html.Div([
+        html.H3("Processable items"),
+        html.Ul([html.Li(filename_or_list)], id="list-container")])
+        return show_items
+
+def resize_to_array(dimensions_selected):
+    choices = {
+        'Photo galleries = 120x90':[120, 90],
+        'Video = 160x120':[160, 120],
+        'Social media profile picture = 100x100':[100, 100],
+        'Online store - small = 80x80':[80, 80],
+        'Online store - large = 150x150':[150, 150],
+        'Icons = 96x96':[96, 96]
+    }
+    dimensions_array = choices[dimensions_selected]
+    return dimensions_array
+
+def generate_payload(filename, dimensions_array):
+    payload = {}
+    payload['filename'] = os.path.join(shared_directory,filename)
+    payload['dimensions'] = dimensions_array
+    return payload
+
+# sends requests async to the backend
+async def async_fetch(payload_dict):
+    payload = payload_dict['data_dict']
+    endpoint = payload_dict['endpoint']
+    
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get(endpoint, data=payload) as response:
+                response.raise_for_status()  # raises an HTTPError for bad responses
+                return await response.json()
+        except aiohttp.ClientError as e:
+            return {'error': str(e)}
+
+async def async_dispatch(list_of_payloads):
+    tasks = [async_fetch(payload_dict) for payload_dict in list_of_payloads]
+    responses = await asyncio.gather(*tasks, return_exceptions=True)
+    return list(zip(list_of_payloads, responses)) # list of tuples;
+
+# result = asyncio.run(async_dispatch(list_of_payloads)) # list of tuples; we only need responses
+
+# loads the new picture as bytes to return it to the user
+def load_picture_as_b(path_to_picture):
+    picture_name = os.path.split(path_to_picture)[1]
+    picture = None
+    with open(path_to_picture,'rb') as hh:
+        picture = hh.read()
+    picture_to_download = [dcc.send_bytes(picture, picture_name)]
+    return picture_to_download
+
+def load_zip_as_b(path_to_zip):
+    # zip_name = os.path.split(path_to_zip)[1]
+    zipfile = None
+    with open(path_to_zip,'rb') as hh:
+        zipfile = hh.read()
+    zipfile_to_download = [dcc.send_bytes(zipfile,'results_zip.zip')]
+    return zipfile_to_download
